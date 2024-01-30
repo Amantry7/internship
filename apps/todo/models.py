1981a597cc4dfdb2063c953ddc25b022f1b37800
Amantry7 <omurzakovomon@gmail.com>
2024-01-30 18:31:38 +0600

    up

--- apps/todo/models.py
+++ apps/todo/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 
 class Task(models.Model):
-
+    id = models.AutoField(
+        primary_key=True
+    )
     title = models.CharField(
         max_length=255,
         verbose_name='Название'
